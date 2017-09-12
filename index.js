b0f5317a79013e5144001d4f25e45091443b13e9
Vernon de Goede <info@vernondegoede.com>
2017-09-12 09:14:57 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const robot = require("robotjs");
+const getColors = require("get-image-colors");
+const Jimp = require("jimp");
+
+const screenCaptureToFile = path =>
+  new Promise((resolve, reject) => {
+    try {
+      const picture = robot.screen.capture();
+      const image = new Jimp(picture.width, picture.height, (error, image) => {
+        image.bitmap.data = picture.image;
+        image.getBuffer(Jimp.MIME_PNG, (err, pngBuffer) => {
+          getColors(pngBuffer, "image/png").then(colors => resolve(colors));
+        });
+      });
+    } catch (e) {
+      reject(e);
+    }
+  });
+
+module.export = screenCaptureToFile();
